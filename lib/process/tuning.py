06c1201a81b293044b6a1ce9575019bdf3e21799
hstehouwer <hstehouwer@jibes.nl>
2022-12-06 18:26:27 +0100

    added recursive alternative for today

--- lib/process/tuning.py
+++ lib/process/tuning.py
@@ -1,4 +1,4 @@
-from typing import Iterator, AnyStr
+from typing import Iterator, AnyStr, List
 
 
 def lockon_tuning(iter: Iterator[AnyStr], target_lenght: int = 4) -> int:
@@ -12,3 +12,12 @@ def lockon_tuning(iter: Iterator[AnyStr], target_lenght: int = 4) -> int:
         comp.pop(0)
         idx += 1
     return idx
+
+
+def rec_lockon_tuning(iter: Iterator[AnyStr], comp: List[AnyStr], target_lenght: int = 4, idx: int = 0) -> int:
+    if len(comp) != target_lenght:  # it's always too short; just append.
+        return rec_lockon_tuning(iter, target_lenght=target_lenght, idx=idx+1, comp=comp + [next(iter)])
+    if len(comp) == len(set(comp)):
+        return idx
+    return rec_lockon_tuning(iter, target_lenght=target_lenght, idx=idx+1, comp=comp[1:] + [next(iter)])
+
--- tests/test_day6.py
+++ tests/test_day6.py
@@ -1,6 +1,6 @@
 import pytest
 
-from lib.process.tuning import lockon_tuning
+from lib.process.tuning import lockon_tuning, rec_lockon_tuning
 
 
 @pytest.mark.parametrize("input,expected_index",[
@@ -12,3 +12,15 @@ from lib.process.tuning import lockon_tuning
 ])
 def test_lockon(input, expected_index):
     assert lockon_tuning(iter(input)) == expected_index
+    assert rec_lockon_tuning(iter(input), []) == expected_index
+
+@pytest.mark.parametrize("input,expected_index",[
+    ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+    ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+    ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+    ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+    ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)
+])
+def test_lockon_part2(input, expected_index):
+    assert lockon_tuning(iter(input), target_lenght=14) == expected_index
+    assert rec_lockon_tuning(iter(input), [], target_lenght=14) == expected_index
