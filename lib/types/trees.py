60938498306c76bcc99c115be4f2a7f155267547
hstehouwer <hstehouwer@jibes.nl>
2022-12-08 22:34:02 +0100

    refactor to no longer hate the code

--- lib/types/trees.py
+++ lib/types/trees.py
@@ -1,19 +1,17 @@
+from enum import IntEnum
 from typing import Iterator, AnyStr, List, Tuple
 from math import prod
 
-directions = [
-    (-1, 0),
-    (1, 0),
-    (0, -1),
-    (0, 1)
-]
+
+class Directions(IntEnum):
+    RIGHT = 0
+    LEFT  = 2
+    UP    = 3
+    DOWN  = 4
 
 
 def lines_to_ints(lines: Iterator[AnyStr]) -> List[List[int]]:
-    ret = []
-    for line in lines:
-        ret.append([int(x) for x in line])
-    return ret
+    return [[int(x) for x in line] for line in lines]
 
 
 def visible_trees(trees: List[List[int]]) -> int:
@@ -25,33 +23,28 @@ def visible_trees(trees: List[List[int]]) -> int:
 
 def visible_tree(trees: List[List[int]], coord: Tuple[int, int]) -> bool:
     return any([
-        is_visible(to_line_of_trees(trees, coord, d)) for d in directions
+        is_visible(to_line_of_trees(trees, coord, d)) for d in Directions
     ])
 
 
-def to_line_of_trees(trees: List[List[int]], coord: Tuple[int, int], direction: Tuple[int, int]) -> List[int]:
-    ret = [trees[coord[0]][coord[1]]]
-    new_coord = (coord[0] + direction[0], coord[1] + direction[1])
-    if not valid_coord(trees, new_coord):
-        return ret
-    return ret + to_line_of_trees(trees, new_coord, direction)
+def to_line_of_trees(trees: List[List[int]], coord: Tuple[int, int], direction: Directions) -> List[int]:
+    match direction:
+        case Directions.RIGHT:
+            return trees[coord[0]][coord[1]:]
+        case Directions.LEFT:
+            return list(reversed(trees[coord[0]][:coord[1]+1]))
+        case Directions.DOWN:
+            return [t[coord[1]] for t in trees][coord[0]:]
+        case Directions.UP:
+            return list(reversed([t[coord[1]] for t in trees][:coord[0]+1]))
 
 
 def is_visible(trees_line: List[int]) -> bool:
-    if len(trees_line) == 1:
-        return True
-    first_tree = trees_line[0]
-    return all(first_tree > t for t in trees_line[1:])
+    return all(trees_line[0] > t for t in trees_line[1:])
 
 
 def valid_coord(trees: List[List[int]], coord: Tuple[int, int]) -> bool:
-    if coord[0] < 0 or coord[1] < 0:
-        return False
-    if coord[0] >= len(trees):
-        return False
-    if coord[1] >= len(trees[0]):
-        return False
-    return True
+    return not(coord[0] < 0 or coord[1] < 0 or coord[0] >= len(trees) or coord[1] >= len(trees[0]))
 
 
 def max_scenic_score(trees: List[List[int]]) -> int:
@@ -63,15 +56,16 @@ def max_scenic_score(trees: List[List[int]]) -> int:
 
 def scenic_score(trees: List[List[int]], coord: Tuple[int,int]) -> int:
     return prod([
-        line_to_score(to_line_of_trees(trees, coord, d)) for d in directions
+        line_to_score(to_line_of_trees(trees, coord, d)) for d in Directions
     ])
 
 
-def line_to_score(line: List[int]) -> int:
-    first_tree = line[0]
-    c = 0
-    for t in line[1:]:
-        c+=1
-        if t >= first_tree:
-            return c
-    return c
+def line_to_score(line: List[int], ft = None, c = 0) -> int:
+    if ft is None:
+        return line_to_score(line[1:], line[0], 0)
+    if not line:
+        return c
+    if line[0] >= ft:
+        return c+1
+    return line_to_score(line[1:], ft, c+1)
+
