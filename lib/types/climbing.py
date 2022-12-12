79efcfc87c7dbd11f1ceabcff82c0b4db2a83758
hstehouwer <hstehouwer@jibes.nl>
2022-12-12 21:51:14 +0100

    No longer bruteforce backwards, now somewhat nice.

--- lib/types/climbing.py
+++ lib/types/climbing.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from dataclasses import dataclass
+from functools import lru_cache
 from queue import PriorityQueue
 from typing import List, AnyStr, Iterator, Callable, Dict
 
@@ -18,7 +19,8 @@ class Coord:
     def __lt__(self, other: 'Coord') -> bool:
         return self.x < other.x or self.x == other.x and self.y < other.y
 
-class Climbing():
+
+class Climbing():gi
     map: List[List[AnyStr]]
     max_x: int
     max_y: int
@@ -30,18 +32,22 @@ class Climbing():
         self.max_x = len(self.map[0])
         self.max_y = len(self.map)
 
+    @lru_cache
     def find_start(self):
         return self.find_coord("S")
 
+    @lru_cache
     def find_end(self):
         return self.find_coord("E")
 
+    @lru_cache
     def find_coord(self, target: AnyStr) -> Coord:
         for y in range(self.max_y):
             for x in range(self.max_x):
                 if self.map[y][x] == target:
                     return Coord(x, y)
 
+    @lru_cache
     def find_all(self, target: AnyStr):
         found = []
         for y in range(self.max_y):
@@ -51,29 +57,38 @@ class Climbing():
         return found
 
     @staticmethod
+    @lru_cache
     def mannhattan(f: Coord, t: Coord) -> int:
         return abs(f.x - t.x) + abs(f.y - t.y)
 
+    @lru_cache
+    def min_mannhattan(self, tgt: AnyStr, t: Coord) -> int:
+        return min([self.mannhattan(t, c) for c in self.find_all(tgt)])
+
     def lenght_shortest_path_to_signal(self) -> int:
-        return self.start_a_star([self.find_start()])
+        path = self.A_Star(
+            start = self.find_start(),
+            goal = lambda x: x == self.find_end(),
+            h = lambda x: self.mannhattan(self.find_end(), x),
+            nbrs=self.get_nbrs
+        )
+        return len(path) - 1
 
     def lenght_scenic_path_to_signal(self) -> int:
-        starts = [self.find_start()] + self.find_all("a")
-        return self.start_a_star(starts)
+        path = self.A_Star(
+            start = self.find_end(),
+            goal = lambda x: self.map[x.y][x.x] == "a",
+            h = lambda x: self.min_mannhattan("a", x),
+            nbrs = self.get_nbrs_reverse
+        )
+        return len(path) - 1
 
-    def start_a_star(self, starts: List[Coord]):
-        scores = []
-        for start in starts:
-            end = self.find_end()
-            goal = lambda x: x == end
-            h = lambda x: self.mannhattan(end, x)
-            # Note: we are looking for the number of steps. So len - 1
-            path = self.A_Star(start, goal, h)
-            if path:
-                scores.append(len(path) - 1)
-        return min(scores)
-
-    def A_Star(self, start: Coord, goal: Callable[[Coord], bool], h: Callable[[Coord], int]) -> List[AnyStr]:
+    def A_Star(self,
+               start: Coord,
+               goal: Callable[[Coord], bool],
+               h: Callable[[Coord], int],
+               nbrs: Callable[[Coord], List[Coord]]
+               ) -> List[AnyStr]:
         open_set = PriorityQueue()
         open_set.put((h(start), start))
         in_open_set = {start}
@@ -87,7 +102,7 @@ class Climbing():
             in_open_set.discard(current)
             if goal(current):
                 return self.reconstruct_path(came_from, current)
-            for nbr in self.get_nbrs(current):
+            for nbr in nbrs(current):
                 tentative_g = g_score[current] + 1
                 if tentative_g < g_score[nbr]:
                     came_from[nbr] = current
@@ -104,15 +119,23 @@ class Climbing():
             path.insert(0, self.map[current.y][current.x])
         return path
 
+    @lru_cache
     def get_nbrs(self, current: Coord) -> List[Coord]:
+        return [c for c in self.get_nbrs_base(current) if self.get_height(c) <= self.get_height(current) + 1]
+
+    @lru_cache
+    def get_nbrs_reverse(self, current: Coord) -> List[Coord]:
+        return [c for c in self.get_nbrs_base(current) if self.get_height(c) >= self.get_height(current) - 1]
+
+    @lru_cache
+    def get_nbrs_base(self, current: Coord) -> List[Coord]:
         candidates = [Coord(current.x+1, current.y),
                       Coord(current.x-1, current.y),
                       Coord(current.x, current.y+1),
                       Coord(current.x, current.y-1)]
-        candidates = [c for c in candidates if 0 <= c.x < self.max_x and 0 <= c.y < self.max_y]
-        candidates = [c for c in candidates if self.get_height(c) <= self.get_height(current) + 1]
-        return candidates
+        return [c for c in candidates if 0 <= c.x < self.max_x and 0 <= c.y < self.max_y]
 
+    @lru_cache
     def get_height(self, coord: Coord) -> int:
         match self.map[coord.y][coord.x]:
             case "S": return 0
